e2c7d48a8d6c73537a32c9ee2439b8639a260d53
Shane Harper <shane@shaneharper.net>
2023-04-01 20:46:53 +1100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Visual_Studio_Warning_Not_Shown_In_Error_List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
